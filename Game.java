c643d189c8954028df8d882e836c9306ee3aa11d
wirasuta <wirasutat@gmail.com>
2019-04-14 11:33:07 +0700

    Translate linkedlist, Game Controller belum selesai

---
+++ Game.java
@@ -0,0 +1,221 @@
+import java.util.Scanner;
+
+/**
+ * Kelas Statik Game.
+ * Sebuah kelas static (TODO) yang menyimpan state dari game dan
+ * method yang digunakan untuk interaksi antar objek pada Engi's Farm
+ */
+public static class Game {
+    private Land[][] landmap;            
+    private Entity[][] entitymap;        
+    private LinkedList<FarmAnimal> animals; 
+    private Truck truck;
+    private Well well;
+    private Mixer mixer;
+    private Player player;
+    private int nBaris,nKolom;
+
+    /**
+     * Initialize Game.
+     * Menerima nama file untuk kemudian dibaca dan dimuat dalam struktur data game.
+     * @param filename nama file eksternal untuk dibaca
+     */
+    public void Initialize(string filename, int xsize, int ysize){
+        nBaris = xsize;
+        nKolom = ysize;
+
+        landmap = new Land[nBaris][nKolom];
+        entitymap = new Entity[nBaris][nKolom];
+
+        BeefRolade.initRecipe();
+        Pancake.initRecipe();
+        GoatCheese.initRecipe();
+        ChickenButterMilk.initRecipe();
+        LoadGame(filename);
+    }
+    /**
+     * Method load game yang akan dipanggil oleh konstruktor.
+     * Membaca kondisi permainan dan memuatnya dalam memori.
+     * @param filename nama file eksternal untuk dibaca
+     */
+    public void LoadGame(string filename){
+        // TODO
+    }
+    /**
+     * Method save game untuk menyimpan kondisi permainan ke file eksternal.
+     * @param filename nama file eksternal yang akan diisi kondisi permainan
+     */
+    public void SaveGame(string filename){
+        //TODO
+    }
+    /**
+     * Method tick untuk menggerakan tick permainan.
+     * Method ini akan memanggil semua method pada elemen map maupun entities yang
+     * berhubungan dengan game tick.
+     */
+    public void Tick(){
+        //Menerima input command
+        Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("> ");
+        String cmd = scanner.nextLine();
+        if (cmd.equals("MOVE")) {
+            player.Move();
+        }else if (cmd.equals("INTERACT")) {
+            player.Interact();
+        }else if (cmd.equals("GROW")) {
+            player.Grow();
+        }else if (cmd.equals("TALK")) {
+            player.Talk();
+        }else if (cmd.equals("STATUS")) {
+            player.PrintStatus();
+        }else if (cmd.equals("KILL")) {
+            player.Kill();
+        }else if (cmd.equals("MIX")) {
+            player.Mix();
+        }
+
+        //Menggerakan semua animal dan mengupdate kondisinya
+        for(FarmAnimal animal : animals){
+            if (animal.GetHungerCountdown() <= -5){
+                animals.remove(animal);
+                setEntity(animal.GetX(),animal.GetY(),null);
+            }else{
+                animal.ReduceHungerCountdown();
+                animal.Move();
+                animal.Eat();
+            }
+        }
+
+        //Mengubah away counter truck
+        truck.TickTruck();
+    }
+    /**
+     * Method draw screen mencetak kondisi permainan.
+     * Method ini memanfaatkan method virtual render dari kelas renderer yang
+     * diturunkan pada semua kelas selain produk.
+     */
+    public void DrawScreen(){
+        for(int i = 0; i < nBaris; i++){
+            for(int j = 0; j < nKolom; j++){
+                if (entitymap[i][j] != null){
+                    System.out.print(entitymap[i][j].Render());
+                }else{
+                    System.out.print(landmap[i][j].Render());
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    /**
+     * Method untuk mengakses land pada posisi x, y.
+     * @param x posisi x land, dimulai dari 0, harus selalu valid
+     * @param y posisi y land, dimulai dari 0, harus selalu valid
+     * @return objek land pada posisi x, y
+     */
+    public Land getLand(int x, int y){
+        return landmap[x][y];
+    }
+    /**
+     * Method untuk menentukan apakah posisi x, y ada dalam map.
+     * @param x , dimulai dari 0
+     * @param y , dimulai dari 0
+     * @return apakah posisi x, y valid
+     */
+    public boolean isValidPosition(int x, int y){
+        return (x >= 0 && y >= 0 && x < nBaris && y < nKolom);
+    }
+    /**
+     * Method untuk menentukan apakah ada entity di posisi x, y
+     * @param x posisi x untuk ditentukan apakah terdapat entity, dimulai dari 0
+     * @param y posisi y untuk ditentukan apakah terdapat entity, dimulai dari 0
+     * @return apakah posisi x,y terdapat entity
+     */
+    public boolean isValidEntity(int x, int y){
+        return entitymap[x][y] != null;
+    }
+    /**
+     * Method untuk mengakses entity pada posisi x, y.
+     * @param x posisi x entity, dimulai dari 0, harus selalu dalam ukuran map
+     * @param y posisi y entity, dimulai dari 0, harus selalu dalam ukuran map
+     * @return objek entity pada posisi x, y. Null jika tidak ada
+     */
+    public Entity getEntity(int x, int y){
+        return entitymap[x][y];
+    }
+    /**
+     * Method untuk mengubah entitymap pada posisi x,y menjadi pointer entity E.
+     * @param x posisi x entity, dimulai dari 0, harus selalu dalam ukuran map
+     * @param y posisi y entity, dimulai dari 0, harus selalu dalam ukuran map
+     * @param E pointer entity, nullptr jika tidak ada
+     * @return objek entity pada posisi x, y
+     */
+    public void setEntity(int x, int y, Entity E){
+        entitymap[x][y] = E;
+    }
+
+    public boolean isAdjacent(int x1, int y1, int x2, int y2){
+        return ((x2 == x1+1 || x2 == x1-1) && y2 == y1) || ((y2 == y1+1 || y2 == y1-1) && x2 == x1);
+    }
+    /**
+     * Method untuk mengakses farmanimal yang memiliki posisi x,y.
+     * @param x posisi x farmanimal, dimulai dari 0 
+     * @param y posisi y farmanimal, dimulai dari 0
+     * @return objek farmanimal pada posisi x, y. Null jika tidak ada
+     */
+    public FarmAnimal getAnimal(int x, int y){
+        for(int i = 0; i < animals.length(); i++){
+            if (animals.get(i).GetX() == x && animals.get(i).GetY() == y){
+                return animals.get(i);
+            }
+        }
+        throw Exception("No animal at the desired location");
+    }
+
+    //TODO : Ubah jadi singleton biar lebih gampang get instancenya
+
+    /**
+     * Method untuk mengakses instans truck jika berada di dekat posisi x,y.
+     * @param x posisi x akses, digunakan untuk menentukan apakah berada di dekat truck 
+     * @param y posisi y akses, digunakan untuk menentukan apakah berada di dekat truck
+     * @return objek truck pada jika berada di dekat x,y. throws exception jika tidak berada di dekat x,y
+     */
+    public Truck getTruck(int x, int y){
+        if (isAdjacent(x,y,truck.GetX(),truck.GetY()))
+            return truck;
+        else
+            throw Exception("Truck is not nearby");
+    }
+    /**
+     * Method untuk mengakses instans well jika berada di dekat posisi x,y.
+     * @param x posisi x akses, digunakan untuk menentukan apakah berada di dekat well 
+     * @param y posisi y akses, digunakan untuk menentukan apakah berada di dekat well
+     * @return objek well pada jika berada di dekat x,y. throws exception jika tidak berada di dekat x,y
+     */
+    public Well getWell(int x, int y){
+        if (isAdjacent(x,y,well.GetX(),well.GetY()))
+            return well;
+        else
+            throw Exception("Well is not nearby");
+    }
+    /**
+     * Method untuk mengakses instans mixer jika berada di dekat posisi x,y.
+     * @param x posisi x akses, digunakan untuk menentukan apakah berada di dekat mixer 
+     * @param y posisi y akses, digunakan untuk menentukan apakah berada di dekat mixer
+     * @return objek mixer pada jika berada di dekat x,y. throws exception jika tidak berada di dekat x,y
+     */
+    public Mixer getMixer(int x, int y){
+        if (isAdjacent(x,y,mixer.GetX(),mixer.GetY()))
+            return mixer;
+        else
+            throw Exception("Mixer is not nearby");
+    }
+    /**
+     * Method untuk mengakses instans player
+     * @return objek player
+     */
+    public Player getPlayer(){
+        return player;
+    }
+
+}
---
+++ LinkedList.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+
+/**
+ * LinkedList
+ */
+public class LinkedList<T> implements Iterable{
+    private int n;
+    private ArrayList<T> list;
+    /**
+     * Ctor linkedlist
+     */
+    public LinkedList() {
+        n = 0;
+        list = new ArrayList<T>();
+    }
+    public LinkedList(LinkedList<T> original) {
+        n = original.n;
+        list = new ArrayList<T>();
+        for (T el : original) {
+            list.add(el);
+        }
+    }
+    public Iterator<T> iterator() {
+        return this.list.iterator();
+    }
+    /**
+     * Mengembalikan indeks dimana elemen ditemukan, -1 jika tidak ada
+     * @param element elemen yang ingin dicari dari linked list
+     * @return index elemen, -1 jika tidak ada
+     */
+    public int find(T element) {
+        if (n > 0){
+            for (int i = 0; i < n; i++) {
+                if (list.get(i).equals(element)){
+                    return i;
+                }
+            }
+            return -1;
+        }else{
+            return -1;
+        }
+    }
+    /**
+     * Mengembalikan True jika linked list kosong
+     * @return apakah linked list kosong
+     */
+    public boolean isEmpty(){ return n == 0; }
+    /**
+     * Mengembalikan panjang linkedlist
+     * @return jumlah elemen pada linkedlist
+     */
+    public int length(){ return n; }
+    /**
+     * Menambahkan elemen sebagai elemen paling akhir
+     * @param elemen elemen yang akan ditambahkan pada linked list
+     */
+    public void add(T element) {
+        list.add(element);
+        n++;
+    }
+    /**
+     * Membuang elemen dari linked list. 
+     * Jika dua elemen yang identik, maka hanya element pertama yang dihapus
+     * @param elemen yang akan dibuang dari linked list
+     */
+    public void remove(T element){
+        list.remove(element);
+    }
+    /**
+     * Mengembalikan elemen pada indeks
+     * @param index index dari data yang diinginkan
+     * @return objek pada index ke-index
+     */
+    public T get(int index) {
+        if (index != -1 && index < n){
+            return list.get(index);
+        }else{
+            throw Exception();
+        }
+    }
+    /**
+     * Mengosongkan linked list
+     */
+    public void removeAll(){
+        if (!isEmpty()){
+            n = 0;
+            list = new ArrayList<T>();
+        }
+    }
+}
