7cffddb341d52633ee8d9e37c2c2a62d406f6416
wirasuta <wirasutat@gmail.com>
2019-04-21 19:02:51 +0700

    Semi final commit

--- src/main/java/engstarsfarm/App.java
+++ src/main/java/engstarsfarm/App.java
@@ -3,14 +3,11 @@ package engstarsfarm;
 import viewcontroller.Game;
 import model.LinkedList;
 import entity.Player;
-import product.*;
 
 public class App{
     public static void main(String[] args){
         try{
             Game.initialize("map/map.dat");
-            Player.getInstance().getInventory().add(new ChickenEgg());
-            Player.getInstance().getInventory().add(new CowMeat());
         } catch (Exception e){
             System.out.println(e);
         }
